d6129cc00fcf1b6dc230d26b20c22f7a5c77567b
joseph Kehoe <joseph.kehoe@itcarlow.ie>
2017-09-25 12:49:05 +0100

    semaphore files

---
+++ Concurrency/Labs/Lab 2:Semaphores/Semaphore.h
@@ -0,0 +1,25 @@
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+/*! \class Semaphore
+    \brief A Semaphore Implementation
+
+   Uses C++11 features such as mutex and condition variables to implement Semaphore
+
+*/
+class Semaphore
+{
+private:
+    unsigned int m_uiCount; /*!< Holds the Semaphore count */
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+
+public:
+    Semaphore(unsigned int uiCount=0)
+          : m_uiCount(uiCount) { };
+    void Wait();
+    template< typename R,typename P >
+    bool Wait(const std::chrono::duration<R,P>& crRelTime);
+    void Signal();
+
+};
