496c98121647cd7637141412c29c8efd0151fdfc
joseph Kehoe <joseph.kehoe@itcarlow.ie>
2017-09-26 14:04:12 +0100

    signal file example

---
+++ Concurrency/Labs/Lab 2:Semaphores/signal.cpp
@@ -0,0 +1,30 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+
+void taskOne(std::shared_ptr<Semaphore> theSemaphore){
+  std::cout <<"I ";
+  std::cout << "must ";
+  std::cout << "print ";
+  std::cout << "first"<<std::endl;
+  theSemaphore->Signal();
+}
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  theSemaphore->Wait();
+  std::cout <<"This ";
+  std::cout << "will ";
+  std::cout << "appear ";
+  std::cout << "second"<<std::endl;
+}
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem( new Semaphore);
+  /**< Launch the threads  */
+  threadOne=std::thread(taskTwo,sem);
+  threadTwo=std::thread(taskOne,sem);
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
