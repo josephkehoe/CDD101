d2d0a88c8764264c6c0b56bc923365bc5cb9f036
josephkehoe <turbogit1>
2018-12-10 14:04:43 +0000

    added new files for reorganisation

---
+++ Concurrency/Labs/Lab 3:Rendezvous/main.cpp
@@ -0,0 +1,39 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <std>
+/*! \class Signal
+    \brief An Implementation of a Rendezvous using Semaphores
+
+   Uses C++11 features such as mutex and condition variables to implement an example of a rendezvous for threads
+
+*/
+/*! displays a message that is split in to 2 sections to show how a rendezvous works*/
+void taskOne(std::shared_ptr<Semaphore> firstSem,std::shared_ptr<Semaphore>  secondSem, int delay){
+  wait(delay);
+  std::cout <<"Task One has arrived! "<< std::endl;
+  //THIS IS THE RENDEZVOUS POINT!
+  std::cout << "Task One has left!"<<std::endl;
+}
+/*! displays a message that is split in to 2 sections to show how a rendezvous works*/
+void taskTwo(std::shared_ptr<Semaphore> firstSem, std::shared_ptr<Semaphore> secondSem, int delay){
+  wait(delay);
+  std::cout <<"Task Two has arrived "<<std::endl;
+  //THIS IS THE RENDEZVOUS POINT!
+  std::cout << "Task Two has left "<<std::endl;
+}
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem1( new Semaphore);
+  std::shared_ptr<Semaphore> sem2( new Semaphore);
+  int taskOneDelay=5;
+  int taskTwoDelay=1;
+  /**< Launch the threads  */
+  threadOne=std::thread(taskTwo,sem1,sem2,taskTwoDelay);
+  threadTwo=std::thread(taskOne,sem1,sem2,taskOneDelay);
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
